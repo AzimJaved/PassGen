b72484749312509892beb6efe414a9766ebecbb3
Azim Javed <thatazimjaved@gmail.com>
2019-05-26 04:22:54 +0530

    Few changes

--- passgen.js
+++ passgen.js
@@ -8,16 +8,26 @@ function getPrime(letter, index){
 }
 
 
-exports.generatePassword = (key, length) => {
+function generatePassword(key, length){
     var primeHash = 1, i=1, pass = '';
     [...key].forEach((c) => {
         primeHash = primeHash*getPrime(c, i)
         i++;
     })
-    primeHash = primeHash.toString()
-    for(i=0; i<2*length; i+=2){
-        var str = parseInt(primeHash.charAt(i) + primeHash.charAt(i+1))
-        pass = pass + charMap[str]
+    for(i=92; i<100;i++){
+        charMap[i]=key.charAt(i-92)
     }
-    console.log("Your Password is "+ pass)
+    primeHash = primeHash.toString()
+    if(primeHash.length >= length*2){
+        for(i=0; i<2*length; i+=2){
+            var str = parseInt(primeHash.charAt(i) + primeHash.charAt(i+1))
+            pass = pass + charMap[str]
+        }
+        console.log("Your Password is "+ pass)
+    }
+    else{
+        console.log("Enter a smaller length/longer key")
+    }
 }
+
+generatePassword(process.argv[2], process.argv[3])
